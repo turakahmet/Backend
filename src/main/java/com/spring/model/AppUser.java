1dfbe863ef05f89d703b688b34e68cdd4cafca4f
efealigulocak <aliefegulocak@gmail.com>
2020-04-10 23:06:52 +0300

    cinli dosyayı kontrol etme pushu

--- src/main/java/com/spring/model/AppUser.java
+++ src/main/java/com/spring/model/AppUser.java
@@ -24,7 +24,7 @@ public class AppUser   {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long userID;
-
+    //cinli dosya comment satırı
     @Column
     private String userName;
 
